e3df270a0970f754c3f03bfac917c867f84422e9
alex <aleks_prg88@mail.ru>
2018-11-14 19:32:06 +0300

    dating

---
+++ Dating/src/MatchMakingTestDrive.java
@@ -0,0 +1,68 @@
+import java.lang.reflect.Proxy;
+
+public class MatchMakingTestDrive {
+
+    public static void main(String[] args) {
+
+        MatchMakingTestDrive test = new MatchMakingTestDrive();
+        test.drive();
+
+    }
+
+    public MatchMakingTestDrive() {
+        initializeDatabase();
+    }
+
+    private void initializeDatabase() {
+    }
+
+    public void drive() {
+
+
+        PersonBean joe = getPersonFromDatabase("Joe Javabean");
+        PersonBean ownerProxy = getOwnerProxy(joe);
+        System.out.println("Name is " + ownerProxy.getName());
+        ownerProxy.setInterests("bowling, Go");
+        System.out.println("Interests set from owner proxy");
+        try {
+            ownerProxy.setHotOrNotRating(10);
+        } catch (Exception e) {
+            System.out.println("Can't set rating from owner proxy");
+        }
+        System.out.println ( "Rating is " + ownerProxy. getHotOrNotRating () );
+        PersonBean nonOwnerProxy = getNonOwnerProxy(joe);
+        System.out.println("Name is " + nonOwnerProxy.getName());
+        try {
+            nonOwnerProxy.setInterests("bowling, Go");
+        } catch (Exception e) {
+            System.out.println("Can't set interests from non owner proxy");
+        }
+        nonOwnerProxy.setHotOrNotRating(3);
+        System.out.println("Rating set from non owner proxy");
+        System.out .println("Rating is " + nonOwnerProxy.getHotOrNotRating());
+
+    }
+
+    private PersonBean getNonOwnerProxy(PersonBean person) {
+        return (PersonBean) Proxy.newProxyInstance (
+                person.getClass().getClassLoader(),
+                person.getClass().getInterfaces(),
+                new NonOwnerInvocationHandler(person));
+
+    }
+
+    private PersonBean getOwnerProxy(PersonBean person) {
+
+        return (PersonBean) Proxy.newProxyInstance (
+                    person.getClass().getClassLoader(),
+                    person.getClass().getInterfaces(),
+                    new OwnerInvocationHandler(person));
+
+    }
+
+    private PersonBean getPersonFromDatabase(String name) {
+
+        return new PersonBeanImpl(name);
+    }
+
+}
---
+++ ImageProxy/src/ImageProxyTestDrive.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class ImageProxyTestDrive {
+
+//    ImageComponent imageComponent ;
+
+    public static void main(String[] args) throws Exception {
+
+        ImageProxyTestDrive testDrive = new ImageProxyTestDrive ();
+
+    }
+
+    public ImageProxyTestDrive() throws Exception {
+//        Icon icon = new ImageProxy(initialURL);
+//        imageComponent = new ImageComponent(icon) ;
+//        frame .getContentPane().add(imageComponent );
+    }
+}
